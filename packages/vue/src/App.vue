5f9dc0a99d5416fd27cb5d6c3e02fe6a7cd0108b
hyl <3307378337@qq.com>
2024-10-10 08:49:16 +0800

    feat

--- packages/vue/src/App.vue
+++ packages/vue/src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <div :class="namespace('preview')">
-    <SmartFilter style="width: 50vw" />
+    <SmartFilter style="width: 50vw" :options="options" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { namespace } from '@smart-filter/core'
 import { SmartFilter } from '@smart-filter/vue'
+
+const options=[
+  {
+    label: '名称',
+    value: 'name'
+  },
+  {
+    label: '状态',
+    value: 'status'
+  },
+  {
+    label: 'ID',
+    value: 'id'
+  }
+]
 </script>
--- packages/vue/src/components/SmartFilter/index.vue
+++ packages/vue/src/components/SmartFilter/index.vue
@@ -6,11 +6,22 @@
         type="text"
         v-model="q"
         placeholder="多个关键字用竖线 “|” 分隔，多个过滤标签用回车键分隔"
+        @focus="onFocus"
+        @blur="onBlur"
+        ref="inputRef"
       />
 
-      <div :class="namespace('inner-dropdown')">
-
-      </div>
+      <ul v-if="visible" :class="namespace('inner-dropdown')">
+        <p :class="namespace('inner-dropdown-title')">请选择过滤类型</p>
+        <li
+          :class="namespace('inner-dropdown-item')"
+          v-for="{ label, value } in options"
+          :key="value"
+          @click="handleSelect(value)"
+        >
+          {{ label }}
+        </li>
+      </ul>
     </div>
 
     <Icon name="search" />
@@ -22,5 +33,32 @@ import { namespace } from '@smart-filter/core'
 import { Icon } from '@smart-filter/vue'
 import { ref } from 'vue'
 
+const props = defineProps<{
+  options: { label: string; value: string }[]
+}>()
+
+const inputRef = ref()
+
 const q = ref('')
+
+const visible = ref(false)
+
+const onFocus = () => {
+  if (q.value) return
+  visible.value = true
+}
+
+const onBlur = () => {
+  // visible.value = false
+}
+
+const handleSelect = (value: string) => {
+  const source = props.options.find((item) => item.value === value)!
+
+  q.value = `${source.label}:`
+
+  visible.value = false
+
+  inputRef.value.focus()
+}
 </script>
